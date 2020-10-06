9da9006db29c33342988050850d225fa6afa9842
Marco Bernasocchi <marco@opengis.ch>
2020-10-06 20:59:58 +0200

    add demo project and compose

--- publish.sh
+++ publish.sh
@@ -1,2 +1,3 @@
+# not needed anymore, there is a github action
 docker build --tag opengisch/qgis-server-standalone .
 docker push opengisch/qgis-server-standalone:latest
---
+++ run.sh
@@ -0,0 +1,2 @@
+docker-compose up -d
+xdg-open "http://localhost:8001/?SERVICE=WMS&REQUEST=GetCapabilities&MAP=/io/data/bees.qgs" &
